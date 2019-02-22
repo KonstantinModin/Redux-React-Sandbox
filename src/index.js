19828e92cb653bf36b26a0866c6c64835943e7a6
Konstantin <konstantin.v.modin@gmail.com>
2019-02-22 13:46:14 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, bindActionCreators } from 'redux';
-import Counter from './counter';
+import { createStore } from 'redux';
+import App from './components/app.js';
+import { Provider } from 'react-redux';
 
 import reducer from './reducer';
-import * as actions from './actions';
+
 
 const store = createStore(reducer);
-const { dispatch } = store;
 
-const { dec, inc, mult, div } = bindActionCreators(actions, dispatch);
-
-const update = () => {
-    ReactDOM.render(
-        <Counter counter={store.getState()}
-        inc={inc}
-        dec={dec}
-        mult={mult}
-        div={() => {
-            const value = Math.floor(Math.random()*10+2);
-            div(value)}} />, 
-        document.querySelector('#root'));
-};
-update();
-store.subscribe(update);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.querySelector('#root'));
 
